9554927ed2601dab0d4e57d934def6dc5d53ac99
dias-royal <jdias@rccl.com>
2025-01-13 14:23:09 -0500

    Initial commit

---
+++ test/recording-analysis.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RecordingAnalysis from '../lib/recording-analysis-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/recording-analysis-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RecordingAnalysis.RecordingAnalysisStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
